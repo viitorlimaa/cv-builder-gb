d3883c121931c7bd3dbd8a8cdc393d010d3b092c
Vitor <vtrlq77@gmail.com>
2025-09-05 17:02:17 -0300

    feat: alterações no design da página de cadastro

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,7 @@ import { PersonalInfo } from "./components/form/PersonalInfo";
 
 function App() {
   return (
-    <div className="p-6">
-      <h1 className="text-2xl font-bold mb-4">CV Builder AI</h1>
+    <div>
       <PersonalInfo />
     </div>
   );

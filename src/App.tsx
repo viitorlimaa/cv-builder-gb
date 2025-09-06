92829ac607639e881f07d1472710f0249352acba
devrichele <brendaricheledev@gmail.com>
2025-09-06 17:08:00 -0300

    feat: adicionar novos componentes de UI
    - Adicionar componentes Skeleton, LoadingSpinner, Toast e ErrorBoundary
    - Remover o arquivo Toast.ts antigo e substituí-lo pela nova implementação de Toast.tsx
    - Configurar Main.tsx como novo ponto de entrada

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { ErrorBoundary } from "./components/ui/ErrorBoundary";
+import LoadingSpinner from "./components/ui/LoadingSpinner";
+import Skeleton from "./components/ui/Skeleton";
+import Toast from "./components/ui/Toast";
+
+function App() {
+  return (
+    <div className="container">
+      {/* <LoadingSpinner />
+      <Skeleton />
+      <ErrorBoundary>
+        <div>
+          <h1>Hello, world!</h1>
+        </div>
+      </ErrorBoundary>
+      <Toast message="Hello, world!" type="success" onClose={() => {}} /> */}
+    </div>
+  );
+}
+
+export default App;
